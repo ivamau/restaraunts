97b36de26367bd803443936e73aeec5ef6da5833
Ivan Maurin <ivamau@utu.fi>
2021-05-04 20:09:17 +0300

    Tehtävät 4.1-4.3

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, FlatList, Button, ActivityIndicator, TextInput, ScrollView } from 'react-native';
+import Header from './components/header';
+
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Wake up', id: '1'},
+    { text: 'Make breakfast', id: '2'},
+    { text: 'Eat the damn breakfast', id: '3'}
+  ]);
+  const [text, setText] = React.useState("");
+  
+  const addTodo = (textToAdd) => {
+    const newTodo = {text: textToAdd, id: Math.floor(Math.random() * 1000).toString()}
+    setTodos(currentTodos => [...currentTodos, newTodo])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          {/* add todo form */}
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <Text>{item.text}</Text>
+              )}
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+              placeholder='Kirjoita muistiinpano tähän'
+            />
+            <Button color='magenta' onPress={() => addTodo(text)} title='add todo' />
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -15,7 +47,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
